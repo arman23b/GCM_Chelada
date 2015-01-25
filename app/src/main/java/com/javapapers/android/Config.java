b5f3e0232b751b18c4a21fef92f695e888feec5a
arman23b <arman23b@gmail.com>
2015-01-25 14:06:19 -0500

    First commit

---
+++ app/src/main/java/com/javapapers/android/Config.java
@@ -0,0 +1,9 @@
+package com.javapapers.android;
+
+public interface Config {
+
+	static final String APP_SERVER_URL = "http://chelada-web.herokuapp.com/testGCM";
+	static final String GOOGLE_PROJECT_NUMBER = "717098562426";
+	static final String MESSAGE_KEY = "message";
+
+}
