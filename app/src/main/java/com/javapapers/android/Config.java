7a59d43bea96afbdf022461c86328617ce88d7a9
arman23b <arman23b@gmail.com>
2015-01-25 15:58:11 -0500

    Changed config and json creation

--- app/src/main/java/com/javapapers/android/Config.java
+++ app/src/main/java/com/javapapers/android/Config.java
@@ -2,8 +2,8 @@ package com.javapapers.android;
 
 public interface Config {
 
-	static final String APP_SERVER_URL = "http://chelada-web.herokuapp.com/testGCM";
+	static final String APP_SERVER_URL = "http://chelada-web.herokuapp.com/gcm/v1/device/register/";
 	static final String GOOGLE_PROJECT_NUMBER = "717098562426";
-	static final String MESSAGE_KEY = "message";
+	static final String MESSAGE_KEY = "msg";
 
 }
--- app/src/main/java/com/javapapers/android/ShareExternalServer.java
+++ app/src/main/java/com/javapapers/android/ShareExternalServer.java
@@ -11,70 +11,85 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import android.content.Context;
+import android.provider.Settings;
 import android.util.Log;
+
+import org.apache.http.message.BasicNameValuePair;
+
 
 public class ShareExternalServer {
 
 	public String shareRegIdWithAppServer(final Context context,
 			final String regId) {
 
+        RestClient client = new RestClient(Config.APP_SERVER_URL);
+
+        String deviceID = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        client.addPostParam(new BasicNameValuePair( "dev_id", deviceID ));
+        client.addPostParam(new BasicNameValuePair("reg_id", regId));
+        client.addPostParam(new BasicNameValuePair("name", "Arman's Phone"));
+        client.execute();
+
 		String result = "";
-		Map<String, String> paramsMap = new HashMap<String, String>();
-		paramsMap.put("regId", regId);
-		try {
-			URL serverUrl = null;
-			try {
-				serverUrl = new URL(Config.APP_SERVER_URL);
-			} catch (MalformedURLException e) {
-				Log.e("AppUtil", "URL Connection Error: "
-						+ Config.APP_SERVER_URL, e);
-				result = "Invalid URL: " + Config.APP_SERVER_URL;
-			}
-
-			StringBuilder postBody = new StringBuilder();
-			Iterator<Entry<String, String>> iterator = paramsMap.entrySet()
-					.iterator();
-
-			while (iterator.hasNext()) {
-				Entry<String, String> param = iterator.next();
-				postBody.append(param.getKey()).append('=')
-						.append(param.getValue());
-				if (iterator.hasNext()) {
-					postBody.append('&');
-				}
-			}
-			String body = postBody.toString();
-			byte[] bytes = body.getBytes();
-			HttpURLConnection httpCon = null;
-			try {
-				httpCon = (HttpURLConnection) serverUrl.openConnection();
-				httpCon.setDoOutput(true);
-				httpCon.setUseCaches(false);
-				httpCon.setFixedLengthStreamingMode(bytes.length);
-				httpCon.setRequestMethod("POST");
-				httpCon.setRequestProperty("Content-Type",
-						"application/x-www-form-urlencoded;charset=UTF-8");
-				OutputStream out = httpCon.getOutputStream();
-				out.write(bytes);
-				out.close();
-
-				int status = httpCon.getResponseCode();
-				if (status == 200) {
-					result = "RegId shared with Application Server. RegId: "
-							+ regId;
-				} else {
-					result = "Post Failure." + " Status: " + status;
-				}
-			} finally {
-				if (httpCon != null) {
-					httpCon.disconnect();
-				}
-			}
-
-		} catch (IOException e) {
-			result = "Post Failure. Error in sharing with App Server.";
-			Log.e("AppUtil", "Error in sharing with App Server: " + e);
-		}
+//		Map<String, String> paramsMap = new HashMap<String, String>();
+//		paramsMap.put("reg_id", regId);
+//        String deviceID = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+//        paramsMap.put("dev_id", deviceID);
+//		try {
+//			URL serverUrl = null;
+//			try {
+//				serverUrl = new URL(Config.APP_SERVER_URL);
+//			} catch (MalformedURLException e) {
+//				Log.e("AppUtil", "URL Connection Error: "
+//						+ Config.APP_SERVER_URL, e);
+//				result = "Invalid URL: " + Config.APP_SERVER_URL;
+//			}
+//
+//			StringBuilder postBody = new StringBuilder();
+//			Iterator<Entry<String, String>> iterator = paramsMap.entrySet()
+//					.iterator();
+//
+//			while (iterator.hasNext()) {
+//				Entry<String, String> param = iterator.next();
+//				postBody.append(param.getKey()).append('=')
+//						.append(param.getValue());
+//				if (iterator.hasNext()) {
+//					postBody.append('&');
+//				}
+//			}
+//			String body = postBody.toString();
+//			byte[] bytes = body.getBytes();
+//			HttpURLConnection httpCon = null;
+//			try {
+//				httpCon = (HttpURLConnection) serverUrl.openConnection();
+//				httpCon.setDoOutput(true);
+//				httpCon.setUseCaches(false);
+//				httpCon.setFixedLengthStreamingMode(bytes.length);
+//				httpCon.setRequestMethod("POST");
+//				httpCon.setRequestProperty("Content-Type",
+//						"application/x-www-form-urlencoded;charset=UTF-8");
+//				OutputStream out = httpCon.getOutputStream();
+//				out.write(bytes);
+//				out.close();
+//
+//				int status = httpCon.getResponseCode();
+//				if (status == 200) {
+//					result = "RegId shared with Application Server. RegId: "
+//							+ regId;
+//				} else {
+//					result = "Post Failure." + " Status: " + status;
+//				}
+//			} finally {
+//				if (httpCon != null) {
+//					httpCon.disconnect();
+//				}
+//			}
+//
+//		} catch (IOException e) {
+//			result = "Post Failure. Error in sharing with App Server.";
+//			Log.e("AppUtil", "Error in sharing with App Server: " + e);
+//		}
 		return result;
 	}
 }
